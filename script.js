7a06b5b7bc94c8d3f271b9d6706012e79867e95a
Abhaya <shankarabhaya9@gmail.com>
2022-11-08 14:02:41 +0530

    Final commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const foodSound = new Audio("../music/food.mp3");
 const gameOverSound = new Audio("../music/gameover.mp3");
 const musicSound = new Audio("../music/music.mp3");
 const moveSound = new Audio("../music/move.mp3");
-const speed = 2;
+const speed = 15;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 5, y: 8 }];
@@ -28,20 +28,35 @@ function main(ctime) {
   gameEngine();
 }
 
-function isCollide(sarr) {
-  return false;
+function isCollide(snake) {
+  // If snake collides with itself.
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+  //If sanke bumps into wall.
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
 }
 
 function gameEngine() {
+  //   musicSound.play();
   // part 1 :  snake array ko update krenge and food render krrenge
-  if (isCollide(sarr)) {
+  if (isCollide(snakeArr)) {
     // if snake collide with the boundary or self collision, then play gameover sound and reset the snake and initial game values.
     gameOverSound.play();
     musicSound.pause();
     inputDir = { x: 0, y: 0 };
     alert("Game over. Press any key to PLAY AGAIN !");
     snakeArr = [{ x: 5, y: 8 }];
-    musicSound.play();
+    // musicSound.play();
     score = 0;
   }
 
@@ -51,6 +66,14 @@ function gameEngine() {
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y,
     });
+    score += 1;
+    if (score > hiscoreval) {
+      hiscoreval = score;
+      localStorage.setItem("hiScore :", JSON.stringify(hiscoreval));
+      hiScoreBox.innerHTML = "HISCORE :" + hiscoreval;
+    }
+    scoreBox.innerHTML = "SCORE : " + score;
+    foodSound.play();
     let a = 2;
     let b = 16;
     food = {
@@ -58,6 +81,14 @@ function gameEngine() {
       y: Math.round(a + (b - a) * Math.random()),
     };
   }
+  // Moving the snake :
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] };
+  }
+
+  snakeArr[0].x += inputDir.x;
+  snakeArr[0].y += inputDir.y;
+
   // part 2 : displaying snake
 
   board.innerHTML = "";
@@ -84,6 +115,15 @@ function gameEngine() {
 }
 
 // Game Logics --
+let hiScore = localStorage.getItem("hiScoreBox");
+if (hiScore === null) {
+  hiscoreval = 0;
+  localStorage.setItem("hiScoreBox", JSON.stringify(hiscoreval));
+} else {
+  hiscoreval = JSON.parse(hiScore);
+  hiScoreBox.innerHTML = "HISCORE :" + hiScore;
+}
+
 window.requestAnimationFrame(main);
 window.addEventListener("keydown", (e) => {
   // initialising keyboard event listener.
